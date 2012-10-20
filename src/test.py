50705dd89e6070678df364cbe72eb99f492af1cf
Amos Joshua <volund@gmail.com>
2012-10-20 20:37:56 +0200

    removed test file

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from FileTypeManager import *
-
-f = FileTypeManager()
-f.register_default_types()
-for fname in ['bla.mp3', 'test.doc', 't.cad']:
-    ftype = f.type_for_file(fname)
-    print("%s: %s | %s" % (fname, ftype.type_id, ftype.metadata_for_file(fname)))
